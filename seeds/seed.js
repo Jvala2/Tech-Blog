e95cd605ad1b2089210cfba507dce8567f6cf203
Jvala2 <joevalancy@gmail.com>
2021-06-15 21:43:59 -0400

    fixed seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,18 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
-
+const seedComments = require('./seedcomment');
+const seedPosts = require('./seedposts');
+const seedUsers = require('./seedusers');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  await seedComments();
+
+  await seedPosts();
+
+  await seedUsers();
 
   await User.bulkCreate(userData, {
     individualHooks: true,
