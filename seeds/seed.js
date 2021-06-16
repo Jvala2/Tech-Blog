11a75b7c87fe4c31b138297ab2368328a9b70304
Jvala2 <joevalancy@gmail.com>
2021-06-15 23:08:04 -0400

    forgot to uncomment

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,24 @@
 const sequelize = require('../config/connection');
 
-const seedComments = require('./seedcomment');
-// const seedPosts = require('./seedposts');
+
 const seedUsers = require('./seedusers');
+const seedComments = require('./seedcomment');
+const seedPosts = require('./seedposts');
+
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  await seedUsers();
+
+  await seedPosts();
+
   await seedComments();
 
- // await seedPosts();
 
-  await seedUsers();
+
+
 
 
   process.exit(0);
