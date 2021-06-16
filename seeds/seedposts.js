f692ddfbccfbc93e371483f23bac1febafae1c7e
Jvala2 <joevalancy@gmail.com>
2021-06-15 20:35:47 -0400

    Does this work seeds?

---
+++ seeds/seedposts.js
@@ -0,0 +1,24 @@
+
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    post_text: 'Nam faucibus, magna vel elementum maximus, magna tellus tincidunt arcu, eu semper justo augue eget est.',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    post_text: 'Donec purus arcu, ultricies dapibus ante et, ullamcorper porta eros.',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    post_text: 'Quisque nisi ante, auctor sed ex quis, consectetur tempor lacus.',
+    user_id: 1
+  },
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
---
+++ seeds/seeduser.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+  {
+    username: 'jojopart6',
+    email: 'jolynecujoh@greendolphin.com',
+    password: '12345'
+  },
+  {
+    username: 'thusspokerohan',
+    email: 'kishibe@moriohmanga.com',
+    password: '12345'
+  },
+  {
+    username: 'yareyaredaze',
+    email: 'jkujo@starfish.com',
+    password: 'password1'
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
