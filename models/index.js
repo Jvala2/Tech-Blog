bb9f6fbf0274d998c3ddf4ef8194b731d3ec9463
Jvala2 <joevalancy@gmail.com>
2021-06-16 17:22:09 -0400

    fixed index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,37 @@
 const User = require('./User');
 const Comment = require('./comment')
 const Post = require('./Post')
+
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
+
 module.exports = { User, Comment, Post };
-
-
