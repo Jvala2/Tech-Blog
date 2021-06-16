e7c47c0e692376bb4704e4f8f6a9570d45b94fb5
Jvala2 <joevalancy@gmail.com>
2021-06-16 17:43:23 -0400

    fixed index.js again?

--- models/index.js
+++ models/index.js
@@ -4,24 +4,24 @@ const Post = require('./Post')
 
 
 
-User.hasMany(Post, {
+/* User.hasMany(Post, {
     foreignKey: 'user_id'
 });
-
-User.hasMany(Comment, {
+*/
+/* User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
-});
+}); */
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+/*
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
     onDelete: 'CASCADE'
-});
+}); */
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
